8e253ecd7b2146a8f02399e92f440f3fdbc903d2
barera14 <alejandro45bernal@gmail.com>
2017-07-06 11:40:36 -0500

    modificacion de textos e item del menu izquierdo

--- Vista/login.php
+++ Vista/login.php
@@ -53,44 +53,33 @@
     <hr>
     <div class="tab-content">
         <div id="login" class="tab-pane active">
-            <form action="index.html">
+            <form action="index.php">
                 <p class="text-muted text-center">
-                    Enter your username and password
+                    Ingrese su Usuario y Contraseña
                 </p>
-                <input type="text" placeholder="Username" class="form-control top">
-                <input type="password" placeholder="Password" class="form-control bottom">
+                <input type="text" placeholder="Usuario" class="form-control top">
+                <input type="password" placeholder="Contraseña" class="form-control bottom">
                 <div class="checkbox">
 		  <label>
-		    <input type="checkbox"> Remember Me
+		    <input type="checkbox"> Recordarme
 		  </label>
 		</div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesion</button>
             </form>
         </div>
         <div id="forgot" class="tab-pane">
-            <form action="index.html">
-                <p class="text-muted text-center">Enter your valid e-mail</p>
-                <input type="email" placeholder="mail@domain.com" class="form-control">
+            <form action="index.php">
+                <p class="text-muted text-center">Ingrese un E-mail Correcto</p>
+                <input type="email" placeholder="mail@example.com" class="form-control">
                 <br>
-                <button class="btn btn-lg btn-danger btn-block" type="submit">Recover Password</button>
+                <button class="btn btn-lg btn-danger btn-block" type="submit">Recuperar Contraseña</button>
             </form>
         </div>
-        <div id="signup" class="tab-pane">
-            <form action="index.html">
-                <input type="text" placeholder="username" class="form-control top">
-                <input type="email" placeholder="mail@domain.com" class="form-control middle">
-                <input type="password" placeholder="password" class="form-control middle">
-                <input type="password" placeholder="re-password" class="form-control bottom">
-                <button class="btn btn-lg btn-success btn-block" type="submit">Register</button>
-            </form>
-        </div>
-    </div>
     <hr>
     <div class="text-center">
         <ul class="list-inline">
-            <li><a class="text-muted" href="#login" data-toggle="tab">Login</a></li>
-            <li><a class="text-muted" href="#forgot" data-toggle="tab">Forgot Password</a></li>
-            <li><a class="text-muted" href="#signup" data-toggle="tab">Signup</a></li>
+            <li><a class="text-muted" href="#login" data-toggle="tab">Iniciar Sesion</a></li>
+            <li><a class="text-muted" href="#forgot" data-toggle="tab">Olvido Contraseña</a></li>
         </ul>
     </div>
   </div>
