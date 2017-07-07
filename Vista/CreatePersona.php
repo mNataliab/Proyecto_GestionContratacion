f4f2d8cdd2ef431fdd11eea77f89233d42bf8a96
barera14 <alejandro45bernal@gmail.com>
2017-07-07 16:26:35 -0500

    Modifico la foto

--- Vista/CreatePersona.php
+++ Vista/CreatePersona.php
@@ -130,9 +130,7 @@
                                             <label class="control-label col-lg-4">Foto</label>
                                             <div class="col-lg-8">
                                                 <div class="fileinput fileinput-new" data-provides="fileinput">
-                                                    <div class="fileinput-new thumbnail" style="width: 200px; height: 150px;">
-                                                        <img data-src="holder.js/100%x100%" alt="...">
-                                                    </div>
+
                                                     <div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 150px;"></div>
                                                     <div>
                                                         <span class="btn btn-default btn-file"><span class="fileinput-new">Select image</span><span class="fileinput-exists">Change</span><input type="file" name="..."></span>
@@ -273,7 +271,7 @@
                         <!-- /.inner -->
                     </div>
                     <!-- /.outer -->
-                </d
+                 </div>
             <!-- /#wrap -->
             <footer class="Footer bg-dark dker">
                 <p>2017 &copy; Metis Bootstrap Admin Template v2.4.2</p>
